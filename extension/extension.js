13f23916c6ed3982a17b3f502fd501106062a3bb
Colton Herschberger <coltonherschberger@gmail.com>
2024-08-08 12:05:25 -0400

    update ignore and title

--- extension/extension.js
+++ extension/extension.js
@@ -1,6 +1,6 @@
 module.exports = {
     "name": "Organization",
-    "publisher": "EckerdCollege",
+    "publisher": "Eckerd College",
     "cards": [{
         "type": "OrganizationTemplate",
         "source": "./src/cards/DepartmentTemplateCard",
